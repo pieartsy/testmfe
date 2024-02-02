a884453f966eb73c309ba5d5cc772ea859ea4e5f
Aster <asterfialla@gmail.com>
2024-02-02 10:55:10 -0500

    removing demo test component

--- testmfe/src/app/app.module.ts
+++ testmfe/src/app/app.module.ts
@@ -4,13 +4,11 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
 import { SixComponent } from './six/six.component';
 import { FormsModule } from "@angular/forms";
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SixComponent,
-    TestComponent,
   ],
   imports: [
     BrowserModule, FormsModule
