001d96f8481590f85acd3528a1781ddedd0c3b2f
Aster <asterfialla@gmail.com>
2024-02-05 19:35:26 -0500

    Getting .NET backend to work

---
+++ TestApi/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace sqltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.ConnectionString = "Server=DESKTOP-BOL7D2M\\MSSQLSERVER16;Database=PetDB;Integrated Security=true;TrustServerCertificate=True;";
+
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+
+                    String sql = "SELECT name, species FROM dbo.Pets;";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nDone. Press enter.");
+            Console.ReadLine();
+        }
+    }
+}
